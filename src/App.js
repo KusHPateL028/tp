8d6ec7cbabbdee44670b3dd14ed13f1d07880b17
Kush Patel <126807075+KusHPateL028@users.noreply.github.com>
2024-04-22 12:18:31 +0530

    sxqs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import moment from 'moment'
 function App() {
+ 
+  const date = moment("12-04-1014");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {date.format('MMMM DD YYYY, h:mm:ss a')}
+    </>
+    
   );
 }
 
