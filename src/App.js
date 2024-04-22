4b7cef7f4b8d0dcb39469276794c120d2ddafe63
Kush Patel <126807075+KusHPateL028@users.noreply.github.com>
2024-04-22 12:20:17 +0530

    ds

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import moment from 'moment'
 function App() {
  
-  const date = moment("12-04-1014");
+  const date = ("12-04-1014");
   return (
     <>
       {date.format('MMMM DD YYYY, h:mm:ss a')}
